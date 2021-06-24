d1ed59d96027ec8318c79d329f251d0384dce66a
劉宜芳 <sweetcat881314@gmail.com>
2021-06-24 19:32:00 +0800

    水果

---
+++ text410728028/text410728028/Controllers/ExamController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace text410728028.Controllers
+{
+    public class ExamController : Controller
+    {
+        // GET: Exam
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
